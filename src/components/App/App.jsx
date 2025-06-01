84cc59c609f6000f5904bddc31075ed9375183b0
Yurii Kit <insidelkit@gmail.com>
2025-06-01 12:03:14 +0300

    start

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,21 +1,9 @@
-// import { useState, useMemo } from 'react';
-// import { useEffect } from 'react';
-// import { useDebounce } from 'use-debounce';
 import ContactForm from '../ContactForm/ContactForm';
 import ContactList from '../ContactList/ContactList';
 import SearchBox from '../SearchBox/SearchBox';
 import css from './App.module.css';
 
 function App() {
-  // const [inputValue, setInputValue] = useState('');
-  // const [debouncedContacts] = useDebounce(inputValue, 1000);
-
-  // const filteredContacts = useMemo(() => {
-  //   return contacts.filter((contact) =>
-  //     contact.name.toLowerCase().includes(debouncedContacts.toLowerCase()),
-  //   );
-  // }, [debouncedContacts, contacts]);
-
   return (
     <>
       <div className={css.container}>
--- src/redux/filtersSlice.js
+++ src/redux/filtersSlice.js
@@ -1,19 +1,4 @@
 import { createSlice } from '@reduxjs/toolkit';
-// import { createAction } from '@reduxjs/toolkit';
-// const initialState = { name: '' };
-// export default function filterReducer(state = initialState, action) {
-//   switch (action.type) {
-//     case 'filters/changeFilter': {
-//       return {
-//         ...state,
-//         name: action.payload,
-//       };
-//     }
-//     default:
-//       return state;
-//   }
-// }
-// export const changeFilter = createAction('filters/changeFilter');
 
 const slice = createSlice({
   name: 'filters',
